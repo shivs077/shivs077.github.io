151ff1dbd7444192105131bb5a530c580bdfca99
Shivam Bisht <31445561+shivs077@users.noreply.github.com>
2019-04-19 13:55:19 +0530

    Updates

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class Footer extends Component {
-  render() {
-    return <footer id="main-footer">Copyright &copy; 2019</footer>;
-  }
-}
