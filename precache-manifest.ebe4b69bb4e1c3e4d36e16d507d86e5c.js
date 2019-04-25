45231398c65415deccbe49190b8f73e3d136c38f
shivs077 <shivambisht.1998@gmail.com>
2019-04-24 20:41:00 -0700

    Updates

---
+++ precache-manifest.ebe4b69bb4e1c3e4d36e16d507d86e5c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0b3912c3e8745bbb1a04842e7a59ff7",
+    "url": "/static/media/portrait1_small.a0b3912c.jpg"
+  },
+  {
+    "revision": "82b415dd1f2af1b03e7e",
+    "url": "/static/css/main.925220ab.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0a7f2648c35ee223ebc7",
+    "url": "/static/js/2.03f24f9c.chunk.js"
+  },
+  {
+    "revision": "a4c62d2ea3cbccb79205d71638a7b50a",
+    "url": "/static/media/portrait1.a4c62d2e.jpg"
+  },
+  {
+    "revision": "c92b85a5b907c70211f4ec25e29a8c4a",
+    "url": "/static/media/favicon.c92b85a5.ico"
+  },
+  {
+    "revision": "82b415dd1f2af1b03e7e",
+    "url": "/static/js/main.9a80d0ec.chunk.js"
+  },
+  {
+    "revision": "00051dbb5b9c7a7f4dfdf04f1b633445",
+    "url": "/static/media/background.00051dbb.jpg"
+  },
+  {
+    "revision": "706be6910a5db9af324332be878555bb",
+    "url": "/static/media/BurgerBuilder.706be691.jpg"
+  },
+  {
+    "revision": "8fadd1af9cf459f3aa91a79264dfc0a9",
+    "url": "/static/media/GoodDrop.8fadd1af.jpg"
+  },
+  {
+    "revision": "6cb74af64909046115967773ec0e0613",
+    "url": "/static/media/HirePredictor.6cb74af6.jpg"
+  },
+  {
+    "revision": "0801387d629ae26b204958de6677681e",
+    "url": "/index.html"
+  }
+];
