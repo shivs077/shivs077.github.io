2386afd98f867390b263a49575ae96238780aaaf
shivs077 <shivambisht.1998@gmail.com>
2019-10-28 17:15:51 +0530

    Updates

--- precache-manifest.3329b0dc1cd6a55a1d6819cc525d6e98.js
+++ precache-manifest.3329b0dc1cd6a55a1d6819cc525d6e98.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "00051dbb5b9c7a7f4dfdf04f1b633445",
-    "url": "/static/media/background.00051dbb.jpg"
+    "revision": "706be6910a5db9af324332be878555bb",
+    "url": "/static/media/BurgerBuilder.706be691.jpg"
   },
   {
-    "revision": "7dfea86757854031abbd",
+    "revision": "da0be8f9df756517b2f0",
     "url": "/static/css/main.5e8b1671.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "77d09d79d21b3cd72531",
-    "url": "/static/js/2.18802a60.chunk.js"
+    "revision": "b4fa176945a01692a160",
+    "url": "/static/js/2.e55ed226.chunk.js"
   },
   {
     "revision": "a4c62d2ea3cbccb79205d71638a7b50a",
@@ -28,24 +28,32 @@ self.__precacheManifest = [
     "url": "/static/media/portrait1_small.a0b3912c.jpg"
   },
   {
-    "revision": "7dfea86757854031abbd",
-    "url": "/static/js/main.73b91ccb.chunk.js"
+    "revision": "00051dbb5b9c7a7f4dfdf04f1b633445",
+    "url": "/static/media/background.00051dbb.jpg"
   },
   {
-    "revision": "706be6910a5db9af324332be878555bb",
-    "url": "/static/media/BurgerBuilder.706be691.jpg"
+    "revision": "da0be8f9df756517b2f0",
+    "url": "/static/js/main.7158fb3e.chunk.js"
   },
   {
-    "revision": "8fadd1af9cf459f3aa91a79264dfc0a9",
-    "url": "/static/media/GoodDrop.8fadd1af.jpg"
+    "revision": "a60844fa57bc21d9b580873d99f66c97",
+    "url": "/static/media/GoodDrop.a60844fa.jpg"
   },
   {
     "revision": "6cb74af64909046115967773ec0e0613",
     "url": "/static/media/HirePredictor.6cb74af6.jpg"
   },
   {
+    "revision": "62ec73907374f0162d87383c01c669e4",
+    "url": "/static/media/VoiceEasy.62ec7390.jpg"
+  },
+  {
     "revision": "59f3976f4383cf3c2989773310b46c89",
     "url": "/static/media/iWeather.59f3976f.jpg"
+  },
+  {
+    "revision": "dc7d9cd1392bcd6e243e695f2aa97456",
+    "url": "/static/media/Soldier.dc7d9cd1.png"
   },
   {
     "revision": "debc9efccf81307a8a972bc5de810e76",
@@ -56,7 +64,7 @@ self.__precacheManifest = [
     "url": "/static/media/Woman.cf7cbc65.png"
   },
   {
-    "revision": "d157d5961b9160deaecd3b8a0e7e4fd8",
+    "revision": "a1d352a2a680dc82c059027173a41503",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0beb2d9c87908274bcaaabd3b8cc1967.js"
+  "/precache-manifest.3329b0dc1cd6a55a1d6819cc525d6e98.js"
 );
 
 workbox.clientsClaim();
