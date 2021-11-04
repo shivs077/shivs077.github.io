82f977f90662ab603dbbb838312f8b15d9ca86c7
Shivam <shivam.bisht@thoughts2binary.com>
2021-11-04 18:36:44 +0530

    build files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import HomePage from './components/home.component';
@@ -7,20 +7,19 @@ import WorkPage from './components/work.component';
 import ContactPage from './components/contact.component';
 import Header from './components/Header';
 
-class App extends Component {
-  render() {
-    return (
-      <Router basename={process.env.PUBLIC_URL}>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/work" component={WorkPage} />
-          <Route path="/contact" component={ContactPage} />
-        </Switch>
-      </Router>
-    );
-  }
-}
+const App = () => {
+  console.log('basename', process.env.PUBLIC_URL);
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/work" component={WorkPage} />
+        <Route path="/contact" component={ContactPage} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
